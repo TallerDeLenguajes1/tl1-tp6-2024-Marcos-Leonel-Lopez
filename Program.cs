1b25259806d26f5078f0492b0279c3cd59d35601
Marcos Lopez <marcosleonellopez@gmail.com>
2024-05-14 15:55:18 -0300

    pnto_1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Ingrese un numero: ");
+
+
+string aux;
+int num,resto;
+int invert =0;
+int c = 1;
+aux = Console.ReadLine();
+num = int.Parse(aux);
+
+Console.WriteLine($"Valor ingresado ={num}");
+
+while(num!= 0){
+    resto = num % 10;
+    num = (int)num / 10;
+    if(c==1)
+    {
+        invert = resto;
+    }
+    else{
+        invert = invert *10 + resto;
+    }
+    c++;
+
+}
+
+Console.WriteLine($"Valor invertido ={invert}");
